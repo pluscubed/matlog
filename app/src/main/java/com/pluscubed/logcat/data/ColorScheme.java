04aaa257e545e4c8ae1bb77b1931495a3782e81d
plusCubed <plusCubed@users.noreply.github.com>
2015-07-09 23:16:24 +0800

    Change color of main text

--- app/src/main/java/com/pluscubed/logcat/data/ColorScheme.java
+++ app/src/main/java/com/pluscubed/logcat/data/ColorScheme.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 public enum ColorScheme {
 	Dark(R.string.pref_theme_choice_dark_value, R.color.main_background_dark,
-			R.color.main_foreground_dark, R.array.dark_theme_colors, R.color.spinner_droptown_dark,
+			R.color.primary_text_default_material_dark, R.array.dark_theme_colors, R.color.spinner_droptown_dark,
 			R.color.main_bubble_background_dark_2, false, R.color.accent),
 	Light (R.string.pref_theme_choice_light_value, R.color.main_background_light, 
 			R.color.main_foreground_light, R.array.light_theme_colors, R.color.spinner_droptown_light,
