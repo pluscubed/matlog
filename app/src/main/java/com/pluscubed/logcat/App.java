4b982d5967fe99ef5992dbcd2cb36857efe27fea
Unpublished <unpublished@gmx.net>
2017-03-07 15:19:13 +0100

    remove fabric for me

--- app/src/main/java/com/pluscubed/logcat/App.java
+++ app/src/main/java/com/pluscubed/logcat/App.java
@@ -3,22 +3,11 @@ package com.pluscubed.logcat;
 import android.app.Application;
 import android.support.v7.app.AppCompatDelegate;
 
-import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.core.CrashlyticsCore;
-
-import io.fabric.sdk.android.Fabric;
-
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
 
-        Crashlytics crashlyticsKit = new Crashlytics.Builder()
-                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
-                .build();
-        Fabric.with(this, crashlyticsKit);
-
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-
     }
 }
