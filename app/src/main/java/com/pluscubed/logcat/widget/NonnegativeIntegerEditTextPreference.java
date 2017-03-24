d396fbeb4daa135e4206c3aaee3e091d325ae2c5
dvdandroid <davide.intelligent@gmail.com>
2017-03-24 16:01:41 +0100

    Use material preferences, fixed white dialog, clean up

--- app/src/main/java/com/pluscubed/logcat/widget/NonnegativeIntegerEditTextPreference.java
+++ app/src/main/java/com/pluscubed/logcat/widget/NonnegativeIntegerEditTextPreference.java
@@ -1,16 +1,17 @@
 package com.pluscubed.logcat.widget;
 
 import android.content.Context;
-import android.preference.EditTextPreference;
 import android.text.method.DigitsKeyListener;
 import android.util.AttributeSet;
+
+import com.afollestad.materialdialogs.prefs.MaterialEditTextPreference;
 
 /**
  * EditTextPreference that only allows inputting integer numbers.
  *
  * @author nlawson
  */
-public class NonnegativeIntegerEditTextPreference extends EditTextPreference {
+public class NonnegativeIntegerEditTextPreference extends MaterialEditTextPreference {
 
     public NonnegativeIntegerEditTextPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
