2b54e5a924a09b2ca8a815ac37289dbb28d9cf8a
veyndan <veyndan89@gmail.com>
2017-04-12 23:58:26 +0100

    Make SavedLog only instantiable once

--- app/src/main/java/com/pluscubed/logcat/data/SavedLog.java
+++ app/src/main/java/com/pluscubed/logcat/data/SavedLog.java
@@ -3,22 +3,20 @@ package com.pluscubed.logcat.data;
 import java.util.List;
 
 public class SavedLog {
-    private List<String> logLines;
-    private boolean truncated;
+
+    private final List<String> logLines;
+    private final boolean truncated;
+
+    public SavedLog(List<String> logLines, boolean truncated) {
+        this.logLines = logLines;
+        this.truncated = truncated;
+    }
 
     public List<String> getLogLines() {
         return logLines;
     }
 
-    public void setLogLines(List<String> logLines) {
-        this.logLines = logLines;
-    }
-
     public boolean isTruncated() {
         return truncated;
-    }
-
-    public void setTruncated(boolean truncated) {
-        this.truncated = truncated;
     }
 }
