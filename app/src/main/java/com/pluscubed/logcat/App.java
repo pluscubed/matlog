f8170d1e195f6d9069472788ab5ee759bc5353d5
SnowVolf <aftervolf@gmail.com>
2017-10-10 15:55:49 +0700

    revert changes in App.java

--- app/src/main/java/com/pluscubed/logcat/App.java
+++ app/src/main/java/com/pluscubed/logcat/App.java
@@ -1,7 +1,6 @@
 package com.pluscubed.logcat;
 
 import android.app.Application;
-import android.content.Context;
 import android.support.v7.app.AppCompatDelegate;
 
 import com.crashlytics.android.Crashlytics;
@@ -10,19 +9,6 @@ import com.crashlytics.android.core.CrashlyticsCore;
 import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
-    private static App instance = null;
-
-    public static App getInstance(){
-        if (instance == null){
-            instance = new App();
-        }
-        return instance;
-    }
-
-    public static Context getContext(){
-        return getInstance();
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
