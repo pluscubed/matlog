984d22bca2257a5bd8c5d9dee73f0ec75a53569e
plusCubed <pluscubed@users.noreply.github.com>
2018-09-16 02:35:31 +0800

    Add F-Droid build script

---
+++ metadata/build-fdroid.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# https://github.com/nutritionfactsorg/daily-dozen-android/blob/master/metadata/build-fdroid.sh
+# https://gitlab.com/fdroid/fdroiddata/blob/master/README.md
+# https://gitlab.com/fdroid/fdroiddata/blob/master/CONTRIBUTING.md
+
+set -ue
+
+appid="com.pluscubed.matlog"
+
+if [[ ! -d fdroidserver ]]; then
+    echo "Cloning fdroidserver"
+    git clone https://github.com/f-droid/fdroidserver.git
+fi
+
+if [[ ! -d fdroiddata ]]; then
+    echo "Cloning fdroiddata"
+    git clone https://github.com/f-droid/fdroiddata.git
+fi
+
+if [[ ! -f fdroiddata/metadata/$appid.txt ]]; then
+    cp $appid.txt fdroiddata/metadata/
+fi
+
+pushd fdroiddata
+
+echo "Create an empty config file"
+touch config.py
+
+echo "Make sure fdroid works and reads the metadata files properly"
+../fdroidserver/fdroid readmeta
+
+echo "Cleaning up metadata file"
+../fdroidserver/fdroid rewritemeta "$appid"
+
+echo "Filling automated fields in metadata file (e.g. Auto Name and Current Version)"
+../fdroidserver/fdroid checkupdates --auto "$appid"
+
+echo "Making sure that fdroid lint doesn't report any warnings. If it does, fix them."
+../fdroidserver/fdroid lint "$appid"
+
+echo "Testing build recipe"
+../fdroidserver/fdroid build -v -l "$appid"
+
+popd
